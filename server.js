a2c36217bbb699cac97834a09e370aa956066783
brengocompany <brengocompany@gmail.com>
2021-08-02 03:14:56 +0700

    how to use graphql

--- server.js
+++ server.js
@@ -1,27 +1,122 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const app = express();
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const port = 4000;
+
+/*==== Database ====*/
+const articles = [
+  {
+    id: 0,
+    title: "Graphql, what's it?",
+    desc: "How to learn graphql and use it with express?",
+    content:
+      "By using the type system, it can be predetermined whether a GraphQL query is valid or not. This allows servers and clients to effectively inform developers when an invalid query has been created, without having to rely on runtime checks",
+  },
+];
+
+/*==== Schema ====*/
+const schema = buildSchema(`
   type Query {
-    hello: String
+    articles: [article]
+    article(id: Int!):article
   }
+
+  type Mutation {
+    updateArticle(id: Int!): mutationResponse!
+    deleteArticle(id: Int!): mutationResponse!
+    createArticle(data: articleContructor!): mutationResponse!
+  }
+
+  enum status {
+    success
+    failure
+  }
+
+  type article {
+    id: Int!
+    title: String
+    desc: String
+    content: String
+  }
+
+  type mutationResponse {
+    status: status!
+    data: article
+  }
+
+  input articleContructor {
+    title: String!
+    desc: String!
+    content: String!
+  }
+
 `);
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
+/*==== Resolve // Root ====*/
+const root = {
+  /*---- resolve (root, context, )  */
+  articles: (_, { articles }) => articles,
+  article: ({ id }, { articles }) =>
+    articles.find((article) => article.id == id),
+  updateArticle: ({ id, data }, { articles }) => {
+    const indexItem = articles.findIndex((article) => article.id == id);
+    if (!indexItem)
+      return {
+        status: "failure",
+        data: null,
+      };
+    articles[indexItem] = { ...articles[indexItem], ...data };
+    return {
+      status: "success",
+      data: articles[indexItem],
+    };
+  },
+  deleteArticle: ({ id }, { articles }) => {
+    const indexItem = articles.findIndex((article) => article.id == id);
+    const data = articles[indexItem];
+    articles.slice(indexItem, indexItem + 1);
+    return {
+      status: "success",
+      data,
+    };
+  },
+  createArticle: ({ data }, { articles }) => {
+    const newData = {
+      ...data,
+      id: articles[articles.length - 1]["id"] + 1,
+    };
+    articles.push(newData);
+    return {
+      status: "success",
+      data: newData,
+    };
   },
 };
 
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+/*==== Router ====*/
+app.use(
+  "/",
+  graphqlHTTP((req, res, { query, variables, operationName, raw }) => {
+    return {
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+      context: {
+        req,
+        res,
+        articles,
+      },
+      extensions: ({ document, variables, operationName, result, cont }) => ({
+        document,
+        variables,
+        operationName,
+        result,
+        cont,
+      }),
+    };
+  })
+);
 
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+app.listen(port, () => console.log(`Example app listening on port port!`));
